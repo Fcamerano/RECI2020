97ffe265d6870d2897597169b1baceb21eed0a1b
Francisco <francamerano@gmail.com>
2020-08-20 19:00:56 -0300

    Primer ej

--- 2020_2Cuatri_Recu_fuentes(noche)/uno.js
+++ 2020_2Cuatri_Recu_fuentes(noche)/uno.js
@@ -9,5 +9,57 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	let i;
+	let nombre;
+	let temperatura;
+	let edad;
+	let sexo;
+	let contadorMujeres = 0;
+	let contadorHombres = 0;
+	let acumuladorEdad = 0;
+	let promedioEdad = 0;
+	let mayorTemperatura = 0;
+	let mujerConMasTemperatura = 0;
+
+
+	for(i=0 ; i<5 ; i++)
+	{
+		do
+    	{
+    		nombre = prompt("Ingrese nombre: ");
+    	}while(!isNaN(nombre));
+		
+		do{
+			temperatura = parseInt(prompt("Ingrese su temperatura: "));
+		}while(temperatura <30 && temperatura > 50);
+
+		do {
+			edad = parseInt(prompt("Ingrese la edad: "));
+		} while (isNaN(edad) && edad>0 && edad<120);
+
+		do {
+			sexo = prompt("Ingrese su sexo: f/m");
+		} while (sexo != "f" && sexo != "m");
+
+		if (sexo == "f")
+		{
+			contadorMujeres++;
+		}
+		else
+		{
+			contadorHombres++;
+		}
+
+		if(mayorTemperatura < temperatura && sexo == "f"){
+			mayorTemperatura = temperatura;
+			mujerConMasTemperatura = nombre;
+	    }
+		acumuladorEdad = acumuladorEdad + edad;
+	}
+
+	promedioEdad = acumuladorEdad / 5;
+
+	console.log("La cantidad de mujeres son: " + contadorMujeres + " y la cantidad de hombres son: " + contadorHombres);
+	console.log("El promedio de edad es: " + promedioEdad);
+	console.log("La mujer con mas temperatura es: " + mujerConMasTemperatura);
 }
